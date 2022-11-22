b740a3ec37c2a6e5e23b3da3e87f8086ca4e23b9
aleksandra-rodzinka <118135484+aleksandra-rodzinka@users.noreply.github.com>
2022-11-22 03:25:31 +0100

    fixing dateElement

--- src/app.js
+++ src/app.js
@@ -13,12 +13,13 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
   let dateElement = document.querySelector("#date");
+  console.log(response.data);
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   conditionsElement.innerHTML = response.data.condition.description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.temperature.humidity;
-  dateElement.innerHTML = formatDate(response.data.time.dt * 1000);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 let ApiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Krakow&key=f0c81td68bc7aeb6ae5b7113o45714af&units=metric";
