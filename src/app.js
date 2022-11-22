a8c9db7912407cee620383ca5b2c5af7e5f986ce
aleksandra-rodzinka <118135484+aleksandra-rodzinka@users.noreply.github.com>
2022-11-22 03:08:14 +0100

    Added more JS elements

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
   let conditionsElement = document.querySelector("#conditions");
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
-  //ddd
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   conditionsElement.innerHTML = response.data.condition.description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.temperature.humidity;
-  console.log(response);
+  dateElement.innerHTML = formatDate(response.data.time.dt * 1000);
 }
 let ApiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Krakow&key=f0c81td68bc7aeb6ae5b7113o45714af&units=metric";
